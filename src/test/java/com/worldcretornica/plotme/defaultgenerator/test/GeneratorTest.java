b3099a4b64bcb2ad33684cacddbff68a5e8c7d18
Matt <mattbdev@outlook.com>
2015-02-10 15:54:13 -0500

    PlotID Test and More UUID Reliance

--- src/test/java/com/worldcretornica/plotme/defaultgenerator/test/GeneratorTest.java
+++ src/test/java/com/worldcretornica/plotme/defaultgenerator/test/GeneratorTest.java
@@ -1,20 +1,18 @@
 package com.worldcretornica.plotme.defaultgenerator.test;
 
+import com.worldcretornica.plotme.defaultgenerator.DefaultPlotManager;
 import org.junit.Assert;
 import org.junit.Test;
-
-import com.worldcretornica.plotme.defaultgenerator.DefaultPlotManager;
 
 public class GeneratorTest {
 
 	@Test
 	public void testGetPlotID() {
-		int road, size, x, z;
 
-		for (road = 3; road < 10; road++) {
-			for (size = 1; size < 30; size++) {
-				for (x = -50; x < 50; x++) {
-					for (z = -50; z < 50; z++) {
+		for (int road = 3; road < 10; road++) {
+			for (int size = 1; size < 30; size++) {
+				for (int x = -50; x < 50; x++) {
+					for (int z = -50; z < 50; z++) {
 						Assert.assertEquals(oldPlotId(road, size, x, z), DefaultPlotManager.internalgetPlotId(road, size, x, z));
 					}
 				}
@@ -23,17 +21,12 @@ public class GeneratorTest {
 	}
 
 	private String oldPlotId(int pathsize, int size, int posx, int posz) {
-		int valx = posx;
-		int valz = posz;
-
-		boolean road = false;
 
 		double n3;
 		int mod2 = 0;
-		int mod1 = 1;
 
-		int x = (int) Math.ceil((double) valx / size);
-		int z = (int) Math.ceil((double) valz / size);
+		int x = (int) Math.ceil((double) posx / size);
+		int z = (int) Math.ceil((double) posz / size);
 
 		if (pathsize % 2 == 1) {
 			n3 = Math.ceil(((double) pathsize) / 2);
@@ -42,20 +35,22 @@ public class GeneratorTest {
 			n3 = Math.floor(((double) pathsize) / 2);
 		}
 
+		boolean road = false;
+		int mod1 = 1;
 		for (double i = n3; i >= 0; i--) {
-			if ((valx - i + mod1) % size == 0 || (valx + i + mod2) % size == 0) {
+			if ((posx - i + mod1) % size == 0 || (posx + i + mod2) % size == 0) {
 				road = true;
-				x = (int) Math.ceil((double) (valx - n3) / size);
+				x = (int) Math.ceil((posx - n3) / size);
 			}
-			if ((valz - i + mod1) % size == 0 || (valz + i + mod2) % size == 0) {
+			if ((posz - i + mod1) % size == 0 || (posz + i + mod2) % size == 0) {
 				road = true;
-				z = (int) Math.ceil((double) (valz - n3) / size);
+				z = (int) Math.ceil((posz - n3) / size);
 			}
 		}
 
 		if (road) {
 			return "";
 		} else
-			return "" + x + ";" + z;
+			return x + ";" + z;
 	}
 }
